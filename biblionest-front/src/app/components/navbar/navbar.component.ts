69e7fd85ac36fd78cce385a34b385d0466b39645
James ABIB <61329786+jabibamman@users.noreply.github.com>
2022-12-26 13:31:55 +0100

    Biblionest Navbar (#6)
    * chore(navbar): added bootstrap v5.2.3
    
    * feat(navbar): responsible navbar done
    
    * chore(navbar): font-awesome icons

--- biblionest-front/src/app/components/navbar/navbar.component.ts
+++ biblionest-front/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { initNavbar } from './agency';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  // TODO: get this value from the server (session) - for now, it's hardcoded
+  isLogged: boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
+    initNavbar();
   }
 
 }
