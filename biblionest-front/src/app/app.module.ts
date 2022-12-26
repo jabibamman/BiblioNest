d77311a60273e6516ac6b0c6a3e2d4ca2615cabb
James ABIB <61329786+jabibamman@users.noreply.github.com>
2022-12-26 16:44:39 +0100

    feat(footer): responsible footer done

--- biblionest-front/src/app/app.module.ts
+++ biblionest-front/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './pages/home/home.component';
-import {RouterModule} from "@angular/router";
+import { RouterModule } from "@angular/router";
 import { BookComponent } from './pages/book/book.component';
 import { AuthentificationComponent } from './pages/authentification/authentification.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -17,12 +18,14 @@ import { ProfileComponent } from './pages/profile/profile.component';
     BookComponent,
     AuthentificationComponent,
     NavbarComponent,
+    FooterComponent,
     ProfileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule
+    RouterModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
