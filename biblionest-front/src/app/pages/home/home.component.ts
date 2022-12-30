1524d25c798b35c7cc706c0c1e9e9997aab48d14
James ABIB <61329786+jabibamman@users.noreply.github.com>
2022-12-30 09:35:31 +0100

    chore(book_display): delete useless desc

--- biblionest-front/src/app/pages/home/home.component.ts
+++ biblionest-front/src/app/pages/home/home.component.ts
@@ -8,9 +8,9 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
   books = [
-    { isbn: '1234567890', title: 'Les Misérables', author: 'Victor Hugo', status: 'to_read', description:'Lorem Ipsum', read_count:0, nb_pages:200, img_url:'https://www.livredepoche.com/sites/default/files/images/livres/couv/9782253096337-001-T.jpeg' },
-    { isbn: '0987654321', title: 'Le Petit Prince', author: 'Antoine de Saint-Exupéry', status: 'read', description:'Lorem Ipsum', read_count:4, nb_pages:200, img_url:'https://m.media-amazon.com/images/I/71lyHAf7XXL.jpg' },
-    { isbn: '1231231231', title: 'Le Rouge et le Noir', author: 'Stendhal', status: 'reading', description:'Lorem Ipsum', read_count:0, nb_pages:200, img_url:'https://www.livredepoche.com/sites/default/files/images/livres/couv/9782253006206-001-T.jpeg' }
+    { isbn: '1234567890', title: 'Les Misérables', author: 'Victor Hugo', status: 'to_read', read_count:0, nb_pages:200, img_url:'https://www.livredepoche.com/sites/default/files/images/livres/couv/9782253096337-001-T.jpeg' },
+    { isbn: '0987654321', title: 'Le Petit Prince', author: 'Antoine de Saint-Exupéry', status: 'read', read_count:4, nb_pages:200, img_url:'https://m.media-amazon.com/images/I/71lyHAf7XXL.jpg' },
+    { isbn: '1231231231', title: 'Le Rouge et le Noir', author: 'Stendhal', status: 'reading', read_count:0, nb_pages:200, img_url:'https://www.livredepoche.com/sites/default/files/images/livres/couv/9782253006206-001-T.jpeg' }
 ];
 
   constructor(private Router: Router) { }
