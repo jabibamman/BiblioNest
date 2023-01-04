a2b67f42a7bbe2e9f78320a60673e9bb093adb08
James ABIB <61329786+jabibamman@users.noreply.github.com>
2023-01-04 21:22:06 +0100

    fix(add_new_book): add a local book fixed

--- biblionest-front/src/app/pages/add-new-book/add-new-book.component.ts
+++ biblionest-front/src/app/pages/add-new-book/add-new-book.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {Router} from "@angular/router";
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { BookService } from 'src/app/service/book.service';
 
 @Component({
   selector: 'app-add-new-book',
@@ -8,45 +9,41 @@ import { FormGroup, FormBuilder } from '@angular/forms';
   styleUrls: ['./add-new-book.component.css']
 })
 export class AddNewBookComponent {
-
   bookForm: FormGroup;
+  books: { isbn: string; title: string; author: string; status: string; read_count: number; nb_pages: number; img_url: string; }[];
 
-  constructor(private router: Router, private fb: FormBuilder) {
+  constructor(private router: Router, private fb: FormBuilder, private BookService: BookService) {
+    this.books = this.books = this.BookService.getBooks();
+
     this.bookForm = this.fb.group({
       title: [''],
       author: [''],
       nb_pages: [1],
+      read_count: [0],
       status: ['to_read'],
       description: ['']
     });
   }
 
-  books = [
-    { id:"lkgznlaizgjnr" ,title: 'Les Misérables', author: 'Victor Hugo', status: 'to_read', description:'Lorem Ipsum', nb_pages:200, img_url:'default' },
-    { id:"e ozejg z" ,title: 'Le Petit Prince', author: 'Antoine de Saint-Exupéry', status: 'read', description:'Lorem Ipsum', nb_pages:200, img_url:'default' },
-    { id:"zegkzoegn" ,title: 'Le Seigneur des anneaux', author: 'Tolkien', status: 'reading', description:'Lorem Ipsum', nb_pages:200, img_url:'default' },
-    { id:"zegzegzg" ,title: 'Les Fleurs du Mal', author: 'Charles Baudelaire', status: 'read', description:'Lorem Ipsum', nb_pages:200, img_url:'default' }
-  ];
-
   addBook(): void{
     const values = this.bookForm.value;
     const book = {
-      id: 'lkgznlaizgjnr',
+      isbn: this.BookService.generateIsbn(),
       title: values.title,
       author: values.author,
       status: values.status,
+      read_count: 0,
       description: values.description,
       nb_pages: values.nb_pages,
-      img_url: 'default'
+      img_url: 'default', 
     };
-    this.books.push(book);
-    console.log(this.books);
-    //this.redirectToHome();
+    this.BookService.addBook(book);
+    this.redirectToHome();
   }
 
   redirectToHome(): void{
     this.router.navigate(['/']);
     return;
   }
-
+  
 }
