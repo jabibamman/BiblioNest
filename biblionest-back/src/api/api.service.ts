12f52d1246577969782aaebd4b61efc1971ee9bf
James ABIB <61329786+jabibamman@users.noreply.github.com>
2023-01-28 18:50:29 +0100

    feat(isbn-api): fixed api usage and enabled cors

--- biblionest-back/src/api/api.service.ts
+++ biblionest-back/src/api/api.service.ts
@@ -4,22 +4,26 @@ import { map, catchError, lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class ApiService {
+    api_url = 'https://www.googleapis.com/books/v1/volumes?q=';
+
     constructor(private http: HttpService) {}
     
     async getBookByISBN(isbn: string) {
       try {
-        const url = `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`;
+        const url = this.api_url + "isbn:" + isbn;
         const get$ = this.http.get(url);
         const { data } = await lastValueFrom(get$);
         if (data.totalItems === 0) {
           throw new Error('Invalid ISBN');
         }
         const book = data.items[0].volumeInfo;
+
         return {
           title: book.title,
           authors: book.authors,
+          publishedDate: book.publishedDate,
           description: book.description,
-          cover: book.imageLinks.thumbnail,
+          cover: (book.imageLinks && book.imageLinks.thumbnail) || 'default',
           isbn: book.industryIdentifiers[0].identifier
         };
       } catch (error) {
@@ -30,7 +34,7 @@ export class ApiService {
 
     async getBookByTitle(title: string) {
       try {
-        const url = `https://www.googleapis.com/books/v1/volumes?q=${title}`;
+        const url = this.api_url + title;
         const get$ = this.http.get(url);
         const { data } = await lastValueFrom(get$);
         if (data.totalItems === 0) {
@@ -40,8 +44,9 @@ export class ApiService {
         return {
           title: book.title,
           authors: book.authors,
+          publishedDate: book.publishedDate,
           description: book.description,
-          cover: book.imageLinks.thumbnail,
+          cover: (book.imageLinks && book.imageLinks.thumbnail) || 'default',
           isbn: book.industryIdentifiers[0].identifier
         };
       } catch (error) {
@@ -51,7 +56,7 @@ export class ApiService {
 
     async getBookByAuthor(author: string) {
       try {
-        const url = `https://www.googleapis.com/books/v1/volumes?q=inauthor:${author}`;
+        const url = this.api_url + "inauthor:" + author;
         const get$ = this.http.get(url);
         const { data } = await lastValueFrom(get$);
         if (data.totalItems === 0) {
@@ -61,14 +66,14 @@ export class ApiService {
         return {
           title: book.title,
           authors: book.authors,
+          publishedDate: book.publishedDate,
           description: book.description,
-          cover: book.imageLinks.thumbnail,
+          cover: (book.imageLinks && book.imageLinks.thumbnail) || 'default',
           isbn: book.industryIdentifiers[0].identifier
         };
       } catch (error) {
         throw new Error(error.message);
       }
     }
-
 
 }
