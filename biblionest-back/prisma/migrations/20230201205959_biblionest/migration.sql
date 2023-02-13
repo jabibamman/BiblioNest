4cfebb96aa12168ed2d1f9de662b401e3d286c69
Carlito0605 <72609695+Carlito0605@users.noreply.github.com>
2023-02-13 14:42:57 +0100

    Api creation (#53)
    * feat(api-creation): API connection + test user auth
    
    * feat(api-creation): added start of signin
    
    * feat(api-creation): fix credentials .env database
    
    * Delete .prettierrc
    
    * chore(api-creation): delete useless com
    
    ---------
    
    Co-authored-by: James ABIB <61329786+jabibamman@users.noreply.github.com>
    Co-authored-by: James <james.bidule@gmail.com>

---
+++ biblionest-back/prisma/migrations/20230201205959_biblionest/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "email" TEXT NOT NULL,
+    "hash" TEXT NOT NULL,
+    "firstName" TEXT,
+    "LastName" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
--- biblionest-back/src/app.module.ts
+++ biblionest-back/src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 import { ApiModule } from './api/api.module';
 
 @Module({
-  imports: [ApiModule],
+    imports: [ApiModule, AuthModule, UserModule, PrismaModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
