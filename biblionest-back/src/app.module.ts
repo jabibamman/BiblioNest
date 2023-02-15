0f3dccdad7f59b35bdff89b5d8a994b04d1b30c3
James ABIB <61329786+jabibamman@users.noreply.github.com>
2023-02-15 21:35:57 +0100

    fix: Add a static function to create DATABASE_URL at the start (#60)

--- biblionest-back/src/app.module.ts
+++ biblionest-back/src/app.module.ts
@@ -5,10 +5,26 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ApiModule } from './api/api.module';
+import { writeFileSync } from 'fs';
 
 @Module({
     imports: [ApiModule, AuthModule, UserModule, PrismaModule],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+    static setupEnv() {
+        const envDir = "../.env"
+        const dotenv = require('dotenv');
+        dotenv.config({ path: envDir });
+        if (process.env.DATABASE_URL === undefined) {
+            writeFileSync(envDir, `\nDATABASE_URL="postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}?schema=public"`, { flag: 'a' });
+        }
+
+        dotenv.config({ path: envDir }); // reload '../.env' file
+
+        if (process.env.DATABASE_URL === undefined) {
+            throw new Error('[ERROR] DATABASE_URL is undefined');
+        }
+    }
+}
