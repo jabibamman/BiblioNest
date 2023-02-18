d73ec79bfe80395b9857d2835d69a4a6245f8517
Ronan Kielt <rkielt@myges.fr>
2023-02-18 15:05:18 +0100

    feat(auth): JwtGuard class

--- biblionest-back/src/user/user.controller.ts
+++ biblionest-back/src/user/user.controller.ts
@@ -1,10 +1,10 @@
 import { Controller, Get, Req, UseGuards } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
 import { Request } from "express";
+import { JwtGuard } from "../auth/guard";
 
 @Controller("profile")
 export class UserController {
-  @UseGuards(AuthGuard(`jwt`))
+  @UseGuards(JwtGuard)
   @Get("") // http://localhost:3000/profile
   getMe(@Req() req: Request) {
     return req.user;
