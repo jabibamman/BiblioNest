98002725dda4ef2a389b8ef7b5cde3b5dacab37a
Ronan Kielt <rkielt@myges.fr>
2023-02-18 19:45:04 +0100

    feat(auth): change http status when sign in

--- biblionest-back/src/auth/auth.controller.ts
+++ biblionest-back/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import {Body, Controller, HttpCode, HttpStatus, Post} from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthDto } from "./dto";
 
@@ -12,6 +12,7 @@ export class AuthController {
     return this.authService.signup(dto);
   }
 
+  @HttpCode(HttpStatus.OK)
   @Post("signin")
   signin(@Body() dto: AuthDto) {
     return this.authService.signin(dto);
--- biblionest-back/src/auth/strategy/jwt.strategy.ts
+++ biblionest-back/src/auth/strategy/jwt.strategy.ts
@@ -2,7 +2,7 @@ import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
-import { PrismaService } from "../prisma/prisma.service";
+import { PrismaService } from "../../prisma/prisma.service";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
