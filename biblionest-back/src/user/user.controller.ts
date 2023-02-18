645c92974916d00d82e7f85e104bcc792245b575
Ronan Kielt <rkielt@myges.fr>
2023-02-18 15:29:09 +0100

    feat(auth): Guard every method in user.controller.ts

--- biblionest-back/src/user/user.controller.ts
+++ biblionest-back/src/user/user.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get, Req, UseGuards } from "@nestjs/common";
-import { Request } from "express";
+import { Controller, Get, UseGuards } from "@nestjs/common";
+import { GetUser } from "../auth/decorator";
 import { JwtGuard } from "../auth/guard";
+import { User } from "@prisma/client";
 
+@UseGuards(JwtGuard)
 @Controller("profile")
 export class UserController {
-  @UseGuards(JwtGuard)
   @Get("") // http://localhost:3000/profile
-  getMe(@Req() req: Request) {
-    return req.user;
+  getMe(@GetUser() user: User) {
+    return user;
   }
 }
