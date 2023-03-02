e00309865f1bcb79f0983dda91ab08b9081c9793
James ABIB <james.bidule@gmail.com>
2023-03-02 08:59:41 +0100

    feat(afficher-les-livres): added book interface

---
+++ biblionest-front/src/app/interface/ibook.ts
@@ -0,0 +1,12 @@
+export interface Book {
+    isbn: string;
+    title: string;
+    author: string;
+    publishedDate: string;
+    status: string;
+    read_count: number;
+    nb_pages: number;
+    description: string;
+    img_url: string;
+  }
+  
